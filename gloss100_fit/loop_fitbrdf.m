11bc4f8b4f0740ddaae1a9800acf41bd30b49c85
gzmk <gizemkucukoglu@gmail.com>
2016-02-02 11:08:22 +0000

    fixed filepath in loop_fitbrdf.m and changed fit params to make it faster

--- gloss100_fit/loop_fitbrdf.m
+++ gloss100_fit/loop_fitbrdf.m
@@ -4,7 +4,7 @@ function loop_fitbrdf(iter)
 
 % init 2 param fitting
 LB_2 = [0.0, 0.01];
-UB_2 = [1.0, 1.0];
+UB_2 = [1.0, 0.3];
 
 
 bestParams = [];
@@ -14,14 +14,14 @@ fitname1 = '100percent_params.mat';
 
 for i = 1:iter
     
-    [XBest, BestF] = gridsearch(@renderIm_2params, LB_2, UB_2, 5, 0.5, 1e-4, 10, 1, 1);
+    [XBest, BestF] = gridsearch(@renderIm_2params, LB_2, UB_2, 5, 0.35, 1e-3, 5, 1, 1);
     sprintf('This is XBest:');
     XBest;
     bestParams = [bestParams;XBest];
     bestfit_2pr = [bestfit_2pr;BestF];
 
     
-    imname = strcat('/scratch/gk925/spheron_brdf_fitting_spray/gloss100_fit/fit_results/multispectral/', fitname1);
+    imname = strcat('/scratch/gk925/spheron_brdf_fitting/gloss100_fit/fit_results/multispectral/', fitname1);
     save(imname, 'bestParams','bestfit_2pr');
     
 end
